3b78c910925d95a9813ae17b238b4c101c27f6b8
Dillo Raju <dilloshion@gmail.com>
2024-08-18 15:59:28 -0400

    Addressing rubocop warnings

--- game.rb
+++ game.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require_relative "mission"
+require_relative 'mission'
 
 class Game
   attr_reader :mission
 
   def initialize
-    puts "Welcome to Mission Control"
+    puts 'Welcome to Mission Control'
   end
 
   def set_mission_name
-    puts "What is the name of this mission?"
+    puts 'What is the name of this mission?'
 
     mission_name = gets.chomp
     @mission = Mission.new(name: mission_name)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,17 +6,17 @@ require_relative '../game'
 RSpec.describe Game do
   subject { described_class.new }
 
-  describe "#initialize" do
-    it "displays a welcome message" do
+  describe '#initialize' do
+    it 'displays a welcome message' do
       expect { subject }.to output("Welcome to Mission Control\n").to_stdout
     end
   end
 
-  describe "#set_mission_name" do
-    it "prompts the user to set the mission name" do
-      allow(subject).to receive(:gets).and_return("Apollo")
+  describe '#set_mission_name' do
+    it 'prompts the user to set the mission name' do
+      allow(subject).to receive(:gets).and_return('Apollo')
       subject.set_mission_name
-      expect(subject.mission.name).to eq("Apollo")
+      expect(subject.mission.name).to eq('Apollo')
     end
   end
 end
